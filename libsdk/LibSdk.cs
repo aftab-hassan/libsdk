1954c99d29a3883bfe73bea489703e8e5f96bc08
Aftab Hassan <afhassan@microsoft.com>
2018-06-21 09:59:24 -0700

    add device and send D2C message

---
+++ libsdk/LibSdk.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace libsdk
+{
+    class LibSdk
+    {
+        public LibSdk()
+        {
+        }
+
+        // Public facing APIs
+        // 1. Add a device
+        public async Task<DeviceInfo> AddDeviceAsync(Endpoint endpoint)
+        {
+            string iotHubConnectionString = endpoint.ConnectionString;
+            string deviceId = endpoint.DeviceId;
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
+
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+
+            List<string> iotHubConnectionStringComponents = endpoint.ConnectionString.Split(';').ToList<string>();
+            string primaryKeyConnectionString = iotHubConnectionStringComponents[0] + ";DeviceId=" + endpoint.DeviceId + ";SharedAccessKey=" + device.Authentication.SymmetricKey.PrimaryKey;
+            
+            return new DeviceInfo(primaryKeyConnectionString);
+        }
+
+        // 2. Send message from device to cloud
+        public Result SendMessageD2CAsync(DeviceInfo deviceInfo, List<Telemetry> data)
+        {
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(deviceInfo.PrimaryKeyConnectionString);
+
+            if (deviceClient == null)
+            {
+                return new Result(false, "Failed to create DeviceClient!");
+            }
+            else
+            {
+                SendEvent(deviceClient, data).Wait();
+                return new Result(false, "Failed to create DeviceClient!");
+            }
+        }
+
+        // 3. Receive desired property change from cloud to device
+        async void ReceiveC2DDesiredPropertyChangeAsync(DeviceInfo deviceInfo, Func<string> callback)
+        {
+
+        }
+
+        // Supporting methods
+        public async Task SendEvent(DeviceClient deviceClient, List<Telemetry> data)
+        {
+            string dataBuffer = "[";
+
+            //for (int i = 0; i < 10; i++)
+            for (int i = 0; i < data.Count; i++)
+            {
+                string dataBufferHere = "{";
+                int keyValuePairCount = 0;
+                foreach (KeyValuePair<string, string> entry in data[i].KeyValuePair)
+                {
+                    dataBufferHere += addProperty(entry.Key, entry.Value);
+
+                    if (keyValuePairCount != (data[i].KeyValuePair.Count - 1))
+                        dataBufferHere += ",";
+
+                    keyValuePairCount++;
+                }
+
+                dataBufferHere += "}";
+                dataBuffer += dataBufferHere;
+
+                if (i != (data.Count - 1))
+                    dataBuffer += ",";
+
+                // Sending each JSON message in loop
+                Microsoft.Azure.Devices.Client.Message eventMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(dataBufferHere));
+                Console.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), 0, dataBufferHere);
+                await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+
+                //// Works small piece
+                //string dataBufferHardCoded = string.Format("{{\"deviceId\":\"{0}\",\"messageId\":{1},\"temperature\":{2},\"humidity\":{3}}}", 0, 1, 98.6, 99.9);
+                //Microsoft.Azure.Devices.Client.Message eventMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(dataBufferHardCoded));
+                //Console.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), i, dataBufferHardCoded);
+                //await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+
+                //// Safe
+                //var temperature = new Random().Next(20, 35);
+                //var humidity = new Random().Next(60, 80);
+                //dataBuffer = string.Format("{{\"deviceId\":\"{0}\",\"messageId\":{1},\"temperature\":{2},\"humidity\":{3}}}", "AftDeviceId1", i, temperature, humidity);
+                //Microsoft.Azure.Devices.Client.Message eventMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(dataBuffer));
+                //eventMessage.Properties.Add("temperatureAlert", (temperature > 25) ? "true" : "false");
+                //Console.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), i, dataBuffer);
+                //await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+            }
+            dataBuffer += "]";
+
+            //// Sending the JSON array
+            //Microsoft.Azure.Devices.Client.Message eventMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(dataBuffer));
+            //Console.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), 0, dataBuffer);
+            //await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+        }
+
+        public async Task SendEvent(DeviceInfo deviceInfo)
+        {
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(deviceInfo.PrimaryKeyConnectionString);
+            string dataBuffer;
+
+            Console.WriteLine("Device sending {0} messages to IoTHub...\n", 5);
+
+            Random rnd = new Random();
+            for (int count = 0; count < 5; count++)
+            {
+                var temperature = rnd.Next(20, 35);
+                var humidity = rnd.Next(60, 80);
+                dataBuffer = string.Format("{{\"deviceId\":\"{0}\",\"messageId\":{1},\"temperature\":{2},\"humidity\":{3}}}", "AftDeviceId1", count, temperature, humidity);
+                Microsoft.Azure.Devices.Client.Message eventMessage = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(dataBuffer));
+                eventMessage.Properties.Add("temperatureAlert", (temperature > 25) ? "true" : "false");
+                Console.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), count, dataBuffer);
+
+                await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+            }
+        }
+
+        public string addProperty(string key, string value)
+        {
+            return "\"" + key + "\":" + "\"" + value + "\"";
+        }
+
+    }
+
+    // Complex types
+    class DeviceInfo
+    {
+        public string PrimaryKeyConnectionString;
+
+        public DeviceInfo(string primaryKeyConnectionString)
+        {
+            this.PrimaryKeyConnectionString = primaryKeyConnectionString;
+        }
+    }
+
+    class Endpoint
+    {
+        public string ConnectionString;
+        public string DeviceId;
+
+        public Endpoint(string connectionString, string deviceId)
+        {
+            this.ConnectionString = connectionString;
+            this.DeviceId = deviceId;
+        }
+    }
+
+    class Telemetry
+    {
+        public Dictionary<string, string> KeyValuePair;
+
+        public Telemetry(Dictionary<string, string> keyValuePair)
+        {
+            this.KeyValuePair = keyValuePair;
+        }
+    }
+
+    class Result
+    {
+        public Boolean IsSuccessful;
+        public string Reason;
+
+        public Result(Boolean isSuccessful, string reason)
+        {
+            this.IsSuccessful = isSuccessful;
+            this.Reason = reason;
+        }
+    }
+}
---
+++ libsdk/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace libsdk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Endpoint endpoint = new Endpoint("HostName=afhassan-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ZqKX5O4OxGf2RNzzZwsIqgV4hYMO9qI3nMJI8SWGcM0=", "AftDeviceId1");
+
+            LibSdk libSdk = new LibSdk();
+
+            // Use case 1 - Add a device
+            Task<DeviceInfo> deviceInfo = libSdk.AddDeviceAsync(endpoint);
+            //deviceInfo.Wait();
+            Console.WriteLine("printing result : " + deviceInfo.Result.PrimaryKeyConnectionString);
+
+            // Use case 2 - Send message from device to cloud
+            List<Telemetry> data = new List<Telemetry>();
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();//first message
+            dictionary.Add("deviceId", "0");
+            dictionary.Add("messageId", "1");
+            dictionary.Add("temperature", "98.6");
+            dictionary.Add("humidity", "99.9");
+            Dictionary<string, string> dictionary1 = new Dictionary<string, string>();//second message
+            dictionary1.Add("deviceId", "000");
+            dictionary1.Add("messageId", "111");
+            dictionary1.Add("temperature", "98.666");
+            dictionary1.Add("humidity", "99.999");
+            Dictionary<string, string> dictionary2 = new Dictionary<string, string>();//third message
+            dictionary2.Add("deviceId", "___000");
+            dictionary2.Add("messageId", "___111");
+            dictionary2.Add("temperature", "___98.666");
+            dictionary2.Add("humidity", "___99.999");
+            Telemetry telemetry = new Telemetry(dictionary);
+            Telemetry telemetry1 = new Telemetry(dictionary1);
+            Telemetry telemetry2 = new Telemetry(dictionary2);
+            data.Add(telemetry);
+            data.Add(telemetry1);
+            data.Add(telemetry2);
+            Result sendMessageResult = libSdk.SendMessageD2CAsync(deviceInfo.Result, data);
+            //sendMessageResult.Wait();
+            //Console.WriteLine("boolean flag : " + sendMessageResult.Result.IsSuccessful + ", reason : " + sendMessageResult.Result.Reason);
+            //libSdk.SendEvent(deviceInfo.Result).Wait();
+
+
+            Console.Read();
+        }
+
+        
+    }
+
+    
+
+
+}
